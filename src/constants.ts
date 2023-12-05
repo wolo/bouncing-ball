124d692a319e61491d3085a2e690810154fe77c2
Wolfgang Loch <wolo@wolosoft.com>
2023-12-05 13:22:36 +0100

    Update kameleoon site code

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,6 @@
 import { KameleoonUtils } from "@kameleoon/javascript-sdk";
 
 // -- Constants values
-export const siteCode = "dhqz9s1tb6";
+export const siteCode = "1ggz4ss5wx";
 export const featureKey = "bouncing_ball_demo";
 export const visitorCode = KameleoonUtils.getVisitorCode("www.example.com");
